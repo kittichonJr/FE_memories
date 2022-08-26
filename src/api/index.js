b8208866b2f491220b1c01c19cfd6849ab069257
kittichon phitakkiri <kittichon.dev@gmail.com>
2022-08-26 21:03:38 +0700

    add new baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3001" });
+const API = axios.create({
+  baseURL: "https://be-memoreis-demo-kittichonjr.vercel.app",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
